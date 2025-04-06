d7a07786d9dc7fcc9c67c4a099beb85699452f78
Hai Vu <haivu2004@gmail.com>
2025-04-06 16:11:02 -0700

    Use list comprehension in flip-matrix

--- flip-matrix/main.py
+++ flip-matrix/main.py
@@ -2,13 +2,14 @@ def flippingMatrix(matrix):
     size = len(matrix)
     half = size // 2
 
-    max_sum = 0
-    for row in range(half):
-        for col in range(half):
-            max_sum += max(
-                matrix[row][col],
-                matrix[row][size - col - 1],
-                matrix[size - row - 1][col],
-                matrix[size - row - 1][size - col - 1],
-            )
+    max_sum = sum(
+        max(
+            matrix[row][col],
+            matrix[row][size - col - 1],
+            matrix[size - row - 1][col],
+            matrix[size - row - 1][size - col - 1],
+        )
+        for row in range(half)
+        for col in range(half)
+    )
     return max_sum
