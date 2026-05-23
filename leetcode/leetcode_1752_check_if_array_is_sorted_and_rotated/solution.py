0f46e3ed97f3530626f26e87d3b2007771256c84
Hai Vu <haivu2004@gmail.com>
2026-05-23 11:50:06 -0700

    chore: add exploratory code

--- leetcode/leetcode_1752_check_if_array_is_sorted_and_rotated/solution.py
+++ leetcode/leetcode_1752_check_if_array_is_sorted_and_rotated/solution.py
@@ -1,6 +1,24 @@
+import logging
 from typing import List
+
+logger = logging.getLogger()
 
 
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        raise NotImplementedError("check")
+        for i, num in enumerate(nums):
+            if i == 0:
+                continue
+            if i < len(nums) - 1 and num > nums[i + 1]:
+                break
+        if i == len(nums) - 1:
+            return True
+        logger.debug(f"{nums=}, {i=}")
+        for i in range(i + 1, len(nums)):
+            try:
+                next_num = nums[i + 1]
+            except IndexError:
+                next_num = nums[0]
+            if nums[i] > next_num:
+                return False
+        return True
