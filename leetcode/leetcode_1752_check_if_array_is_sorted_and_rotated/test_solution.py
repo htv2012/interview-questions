d8b8dddc96c8260747c490811da653a36fe5b3f3
Hai Vu <haivu2004@gmail.com>
2026-05-23 14:11:19 -0700

    feat: add leetcode 1752 (#6)
    * feat: add leetcode 1752
    
    * chore: organize files
    
    * chore: add exploratory code
    
    * chore: complete the solution
    
    * refactor: optimize against too many inconsistencies

---
+++ leetcode/leetcode_1752_check_if_array_is_sorted_and_rotated/test_solution.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+"""
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    ["nums", "expected"],
+    [
+        pytest.param([3, 4, 5, 1, 2], True, id="example 1"),
+        pytest.param([2, 1, 3, 4], False, id="example 2"),
+        pytest.param([1, 2, 3], True, id="example 3"),
+        pytest.param([1, 2, 1, 2, 1, 2], False, id="too many inconsistencies"),
+        pytest.param([1], True, id="one element"),
+        pytest.param([], True, id="empty list"),
+    ],
+)
+def test_solution(fut, nums, expected):
+    assert fut(nums) == expected
