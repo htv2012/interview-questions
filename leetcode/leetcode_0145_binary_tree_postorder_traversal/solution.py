c85849d45408508be746ce5809125f5a762e048c
Hai Vu <haivu2004@gmail.com>
2026-05-23 22:55:07 -0700

    chore: remove commented code

--- leetcode/leetcode_0145_binary_tree_postorder_traversal/solution.py
+++ leetcode/leetcode_0145_binary_tree_postorder_traversal/solution.py
@@ -2,13 +2,6 @@ from typing import List, Optional
 
 from tree import TreeNode
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 
 def post_order(root: Optional[TreeNode]) -> List[int]:
     out = []
