9bf163c4dd1067f9bf4dcffe8c91ece2557e6349
Hai Vu <haivu2004@gmail.com>
2026-06-16 12:13:02 -0700

    chore: implement

--- leetcode/leetcode_3612_process_string_with_special_operations_i/solution.py
+++ leetcode/leetcode_3612_process_string_with_special_operations_i/solution.py
@@ -1,3 +1,16 @@
 class Solution:
     def processStr(self, s: str) -> str:
-        raise NotImplementedError('processStr')
+        out = []
+        for c in s:
+            if c == "*":
+                try:
+                    out.pop()
+                except IndexError:
+                    pass
+            elif c == "#":
+                out.extend(out)
+            elif c == "%":
+                out.reverse()
+            else:
+                out.append(c)
+        return "".join(out)
--- leetcode/leetcode_3612_process_string_with_special_operations_i/test_solution.py
+++ leetcode/leetcode_3612_process_string_with_special_operations_i/test_solution.py
@@ -6,25 +6,34 @@ import types
 
 import pytest
 
+from solution import Solution
+
+
+@pytest.fixture
+def fut():
+    """Function under test."""
+    sol = Solution()
+    return sol.processStr
+
 
 @pytest.mark.parametrize(
     "test_case",
     [
         pytest.param(
             types.SimpleNamespace(
-                s='a#b%*',
-                expected='ba',
+                s="a#b%*",
+                expected="ba",
             ),
-            id='Example 1',
+            id="Example 1",
         ),
         pytest.param(
             types.SimpleNamespace(
-                s='z*#',
-                expected='',
+                s="z*#",
+                expected="",
             ),
-            id='Example 2',
+            id="Example 2",
         ),
-    ]
+    ],
 )
 def test_solution(fut, test_case):
-    pass
+    assert fut(test_case.s) == test_case.expected
