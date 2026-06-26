ca55e924e241f72a68a5fa0454d6f522b5fd1538
Hai Vu <haivu2004@gmail.com>
2026-06-26 00:05:50 -0700

    refactor: simplify logic

--- leetcode/leetcode_3467_transform_array_by_parity/solution.py
+++ leetcode/leetcode_3467_transform_array_by_parity/solution.py
@@ -6,11 +6,11 @@ logger = logging.getLogger()
 
 class Solution:
     def transformArray(self, nums: List[int]) -> List[int]:
-        # count even numbers
         odd_count = sum(num % 2 for num in nums)
         even_count = len(nums) - odd_count
         logger.debug(f"{nums=}, {even_count=}, {odd_count=}")
-        for i, _ in enumerate(nums):
-            nums[i] = int(i >= even_count)
+
+        nums[:even_count] = [0] * even_count
+        nums[even_count:] = [1] * odd_count
 
         return nums
