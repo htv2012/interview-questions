0d32232d1fa06d7772444b75fd88507f479de81b
Hai Vu <haivu2004@gmail.com>
2026-06-27 17:46:07 -0700

    chore: make attempts, but no solution yet

--- leetcode/leetcode_0725_split_linked_list_in_parts/solution.py
+++ leetcode/leetcode_0725_split_linked_list_in_parts/solution.py
@@ -18,15 +18,50 @@ def count_nodes(head: Optional[ListNode]) -> int:
     return nodes_count
 
 
+def divide(count: int, parts) -> list:
+    """
+    Divide count into parts.
+
+    >>> divide(35, 7)
+    [5, 5, 5, 5, 5, 5, 5]
+
+    >>> divide(37, 7)
+    [6, 6, 5, 5, 5, 5, 5]
+
+    >>> divide(25, 4)
+    [7, 6, 6, 6]
+
+    >>> divide(3, 1)
+    [3]
+
+    >>> divide(3, 7)
+    [1, 1, 1, 0, 0, 0, 0]
+    """
+    quotient, remainder = divmod(count, parts)
+    return [quotient + 1 if i < remainder else quotient for i in range(parts)]
+
+
 class Solution:
     def splitListToParts(
         self, head: Optional[ListNode], k: int
     ) -> List[Optional[ListNode]]:
-        out = [None] * k
-        nodes_count = count_nodes(head)
         if k == 1:
             return [head]
+
+        out = [None] * k
+        nodes_count = count_nodes(head)
         if nodes_count == 0:
             return out
 
-        return [nodes_count]
+        node = head
+        for seg, count in zip(range(k), divide(nodes_count, k)):
+            prev = None
+            for _ in range(count):
+                prev = node
+                if node is None:
+                    break
+                node = node.next
+
+            prev.next = None
+
+        return out
