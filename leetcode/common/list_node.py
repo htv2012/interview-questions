44902ddb98ceeff434116cd63472995a9a64d237
Hai Vu <haivu2004@gmail.com>
2026-06-28 09:19:44 -0700

    feat: add count_nodes (#18)
    - refactor: repr

--- leetcode/common/list_node.py
+++ leetcode/common/list_node.py
@@ -1,9 +1,10 @@
 import itertools
-import itertools
-from typing import Optional
 import logging
+import reprlib
+from typing import Optional
 
 logger = logging.getLogger()
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -11,13 +12,13 @@ class ListNode:
         self.next = next
 
     def __repr__(self):
-        # return f"<ListNode {self.val}>"
         values = []
         node = self
         while node:
             values.append(node.val)
             node = node.next
-        return f"<ListNode val={self.val}, nodes={values}>"
+        values = reprlib.repr(values)
+        return values.replace("[", "<ListNode ").replace("]", ">")
 
     def __iter__(self):
         node = self
@@ -53,14 +54,20 @@ def iter_list(head: ListNode):
         head = head.next
 
 
+def count_nodes(head: Optional[ListNode]) -> int:
+    return len(list(iter_list(head)))
+
+
 def assert_values(head: ListNode, expected: list):
     logger.debug(f"assert_values({head=}, {expected=})")
     node = head
     for node_number, expected_value in enumerate(expected):
-        assert node is not None, f"Expect node[{node_number}]={expected_value!r}, but the node is None"
-        assert (
-            node.val == expected_value
-        ), f"Node number: {node_number}, {expected_value=}, {node.val=}"
+        assert node is not None, (
+            f"Expect node[{node_number}]={expected_value!r}, but the node is None"
+        )
+        assert node.val == expected_value, (
+            f"Node number: {node_number}, {expected_value=}, {node.val=}"
+        )
         node = node.next
     assert node is None
 
@@ -73,9 +80,16 @@ def verify_values(head: Optional[ListNode], expected_values: list) -> bool:
             logger.debug("list is too short, missing %r", expected_values[index:])
             return False
         elif expected_value is marker:
-            logger.debug("list is too long, has extra %r", [n.val for n in iter_list(node)])
+            logger.debug(
+                "list is too long, has extra %r", [n.val for n in iter_list(node)]
+            )
             return False
         elif node.val != expected_value:
-            logger.debug("at node #%d, expected=%r, but actual=%r", index, expected_value, node.val)
+            logger.debug(
+                "at node #%d, expected=%r, but actual=%r",
+                index,
+                expected_value,
+                node.val,
+            )
             return False
     return True
