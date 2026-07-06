dee8f2fd3920c48e81de3373e00737e54f95307b
Hai Vu <haivu2004@gmail.com>
2026-07-05 18:51:46 -0700

    test: correct tests for get_inorder_successor

--- leetcode/leetcode_0450_delete_node_in_a_bst/test_successor.py
+++ leetcode/leetcode_0450_delete_node_in_a_bst/test_successor.py
@@ -10,9 +10,13 @@ from solution import get_inorder_successor
 
 @parametrize("successor.yaml", ["root", "expected"])
 def test_successor(root, expected):
-    root_tree = tree.breadth_first_build(root)
     expected_tree = tree.breadth_first_build(expected[0])
     expected_parent = tree.breadth_first_build(expected[1])
-    actual_tree, actual_parent = get_inorder_successor(root_tree)
+    expected_side = expected[2]
+
+    root_tree = tree.breadth_first_build(root)
+    actual_tree, actual_parent, actual_side = get_inorder_successor(root_tree)
+
     assert tree.compare_trees(actual_tree, expected_tree)
     assert tree.compare_trees(actual_parent, expected_parent)
+    assert actual_side == expected_side
