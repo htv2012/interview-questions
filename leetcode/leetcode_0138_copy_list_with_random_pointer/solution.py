8cc174d3171ade6c5aa326e088a01ce649bbc0ef
Hai Vu <haivu2004@gmail.com>
2026-07-18 22:58:15 -0700

    chore: implement and test

--- leetcode/leetcode_0138_copy_list_with_random_pointer/solution.py
+++ leetcode/leetcode_0138_copy_list_with_random_pointer/solution.py
@@ -3,7 +3,7 @@ from typing import Optional
 from node import Node
 
 
-def iter_nodes(head: Optional[Node]):
+def iter_nodes(head: "Optional[Node]"):
     node = head
     counter = 0
     while node is not None:
@@ -14,5 +14,17 @@ def iter_nodes(head: Optional[Node]):
 
 class Solution:
     def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
-        node_list = [Node(node.val) for _, node in iter_nodes(head)]
+        index = {node: i for i, node in iter_nodes(head)}
+        index[None] = len(index)
+        in_list = [[node.val, index[node.random]] for _, node in iter_nodes(head)]
 
+        li = [Node(val) for val, _ in in_list]
+        li.append(None)  # Last element: The null node
+
+        for i in range(len(li) - 1):
+            li[i].next = li[i + 1]
+
+        for (_, random_index), node in zip(in_list, li):
+            node.random = li[random_index]
+
+        return li[0]
