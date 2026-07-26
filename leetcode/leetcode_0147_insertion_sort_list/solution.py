9faf1b3326ecd4b7f5b2b2b66cfe96fdaa846aec
Hai Vu <haivu2004@gmail.com>
2026-07-26 15:18:59 -0700

    chore: re-word comments

--- leetcode/leetcode_0147_insertion_sort_list/solution.py
+++ leetcode/leetcode_0147_insertion_sort_list/solution.py
@@ -3,19 +3,19 @@ from list_node import ListNode
 
 class Solution:
     def insertionSortList(self, head: ListNode | None) -> ListNode | None:
-        # pre_head a dummy node, points to the new head
+        # Dummy node pre_head points to the new head
         pre_head = ListNode(-1)
 
         node = head
         while node is not None:
             next_node = node.next
 
-            # Find the right insertion point
+            # Find the insertion point
             left, right = pre_head, pre_head.next
             while right is not None and right.val < node.val:
                 left, right = right, right.next
 
-            # Insert new node between left and right pointers
+            # Insert the new node between the left- and right pointers
             left.next = node
             node.next = right
 
