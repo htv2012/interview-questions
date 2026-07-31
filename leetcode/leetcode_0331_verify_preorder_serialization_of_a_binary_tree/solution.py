4b3d125ae056c1a84754827226c347119e19f1a8
Hai Vu <haivu2004@gmail.com>
2026-07-30 21:40:57 -0700

    chore: remove logging to improve performance

--- leetcode/leetcode_0331_verify_preorder_serialization_of_a_binary_tree/solution.py
+++ leetcode/leetcode_0331_verify_preorder_serialization_of_a_binary_tree/solution.py
@@ -1,27 +1,17 @@
-import logging
-
-logger = logging.getLogger()
-
-
 NULL = "#"
 
 
 def is_valid(tokens: list[str]) -> bool:
-    logger.debug(f"is_valid({tokens})")
-
     stack = []
     for token in tokens:
         stack.append(token)
-        logger.debug(f"{stack=}")
         while (
             len(stack) > 2
             and stack[-1] == NULL
             and stack[-2] == NULL
             and stack[-3] != NULL
         ):
-            logger.debug(f"Leaf found: {stack}")
             stack[-3:] = [NULL]
-            logger.debug(f"  stack becomes {stack}")
 
     return stack == [NULL]
 
