a7ec28421b55639a8c8a9f9e2a541cb1d5f61e79
Hai Vu <haivu2004@gmail.com>
2026-08-13 12:08:32 -0700

    feat: add leetcode 365

---
+++ leetcode/leetcode_0365_water_and_jug_problem/test_solution.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/water-and-jug-problem/
+"""
+
+import pytest
+
+from solution import Solution
+
+
+@pytest.mark.parametrize(
+    "x, y, target, expected",
+    [
+        pytest.param(3, 5, 4, True, id="Example 1"),
+        pytest.param(2, 6, 5, False, id="Example 2"),
+        pytest.param(1, 2, 3, True, id="Example 3"),
+    ],
+)
+def test_solution(x, y, target, expected):
+    sol = Solution()
+    assert sol.canMeasureWater(x, y, target) == expected
