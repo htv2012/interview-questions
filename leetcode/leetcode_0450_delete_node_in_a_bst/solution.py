9fb9e016e93563822ce365da6fa6779ea37dbf7c
Hai Vu <haivu2004@gmail.com>
2026-08-26 07:28:13 -0700

    fix: fix leetcode 450 to make it works

--- leetcode/leetcode_0450_delete_node_in_a_bst/solution.py
+++ leetcode/leetcode_0450_delete_node_in_a_bst/solution.py
@@ -1,27 +1,29 @@
-import logging
 from typing import Optional
 
 from tree import TreeNode
-
-logger = logging.getLogger()
 
 
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if root is None:
             return None
-        elif root.left is None and root.right is None:
-            return None
         elif key < root.val:
             root.left = self.deleteNode(root.left, key)
-            return root.left
+            return root
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
-            return root.right
-        else:
-            replacement = root.right
-            while replacement.left:
-                replacement = replacement.left
-            replacement.left = root.left
-            root = root.right
             return root
+
+        # Gets here means root.val == key, root is the node we want to delete
+        if root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        else:
+            # Find the successor: the left most node of the right sub-tree
+            successor = root.right
+            while successor.left is not None:
+                successor = successor.left
+
+            successor.left = root.left
+            return root.right
--- leetcode/leetcode_0450_delete_node_in_a_bst/test_delete.py
+++ leetcode/leetcode_0450_delete_node_in_a_bst/test_delete.py
@@ -2,28 +2,27 @@
 https://leetcode.com/problems/delete-node-in-a-bst/
 """
 
-import pytest
+import logging
+
 import tree
 
 from solution import Solution
 from testlib import log_tree, parametrize
 
-
-@pytest.fixture
-def fut():
-    sol = Solution()
-    return sol.deleteNode
+logger = logging.getLogger()
 
 
 @parametrize("delete.yaml", ["root", "key", "expected"])
-def test_delete(fut, root, key, expected):
+def test_delete(root, key, expected):
+    sol = Solution()
     root_tree = tree.breadth_first_build(root)
-    log_tree(root_tree, "Root Tree")
-
     expected_tree = tree.breadth_first_build(expected)
+
+    logger.debug(f"{key = }")
+    log_tree(root_tree, "Root Tree")
     log_tree(expected_tree, "Expected Tree")
 
-    actual_tree = fut(root_tree, key)
+    actual_tree = sol.deleteNode(root_tree, key)
     log_tree(actual_tree, "Actual Tree")
 
     assert tree.compare_trees(actual_tree, expected_tree)
