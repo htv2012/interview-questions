90a40308811f46bd0872a6d3ee306e40b5f21bb5
Hai Vu <haivu2004@gmail.com>
2026-09-03 00:01:21 -0700

    performance tune leetcode_2129_capitalize_the_title

--- leetcode/leetcode_2129_capitalize_the_title/solution.py
+++ leetcode/leetcode_2129_capitalize_the_title/solution.py
@@ -1,30 +1,4 @@
-import logging
-
-logger = logging.getLogger("solution")
-
-
-def cap(word: str) -> str:
-    out = word.lower() if len(word) <= 2 else word.title()
-    logger.debug(f"cap {word!r} -> {out!r}")
-    return out
-
-
 class Solution:
     def capitalizeTitle(self, title: str) -> str:
-        start, end = 0, None
-        out = []
-
-        while start < len(title):
-            try:
-                end = title.index(" ", start)
-            except ValueError:
-                # no more spaces
-                end = len(title)
-            out.append(cap(title[start:end]))
-            while end < len(title) and title[end] == " ":
-                out.append(title[end])
-                end += 1
-            start = end
-
-        logger.debug(f"exit while, {out = }")
-        return "".join(out)
+        out = " ".join(w.lower() if len(w) <= 2 else w.title() for w in title.split())
+        return out
