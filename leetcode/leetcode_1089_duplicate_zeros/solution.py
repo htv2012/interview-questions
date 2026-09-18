c4becdac57f8e732530be3df25911b12e81552c7
Hai Vu <haivu2004@gmail.com>
2026-09-18 09:39:36 -0700

    refactor: optimize

--- leetcode/leetcode_1089_duplicate_zeros/solution.py
+++ leetcode/leetcode_1089_duplicate_zeros/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def duplicateZeros(self, arr: list[int]) -> None:
+        def copy():
+            if (dest := source + zeros_count) < size:
+                arr[dest] = arr[source]
+
         zeros_count = arr.count(0)
         size = len(arr)
 
         for source in range(size - 1, -1, -1):
-            # Copy
-            if (dest := source + zeros_count) < size:
-                arr[dest] = arr[source]
-
-            # Copy again if source contains zero
+            copy()
             if arr[source] == 0:
                 zeros_count -= 1
-                if (dest := source + zeros_count) < size:
-                    arr[dest] = 0
+                copy()
