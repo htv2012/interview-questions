3ff66e8053d57eb91241dad2dd00a89a2260a6b3
Hai Vu <haivu2004@gmail.com>
2026-09-18 10:26:49 -0700

    add more test cases leetcode_1089_duplicate_zeros

--- leetcode/leetcode_1089_duplicate_zeros/test_solution.py
+++ leetcode/leetcode_1089_duplicate_zeros/test_solution.py
@@ -15,6 +15,11 @@ from solution import Solution
         ),
         pytest.param([1, 2, 3], [1, 2, 3], id="Example 2"),
         pytest.param([1, 0, 2, 0, 3], [1, 0, 0, 2, 0], id="spill over"),
+        pytest.param([], [], id="empty array"),
+        pytest.param([1], [1], id="1"),
+        pytest.param([0], [0], id="0"),
+        pytest.param([0, 1], [0, 0], id="0, 1"),
+        pytest.param([1, 0], [1, 0], id="1, 0"),
     ],
 )
 def test_solution(arr, expected):
