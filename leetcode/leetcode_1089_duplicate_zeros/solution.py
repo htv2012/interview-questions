43b9e720234d99a6840bfd99b2a10b3cc37bce5a
Hai Vu <haivu2004@gmail.com>
2026-09-18 09:29:09 -0700

    refactor: simplify code

--- leetcode/leetcode_1089_duplicate_zeros/solution.py
+++ leetcode/leetcode_1089_duplicate_zeros/solution.py
@@ -5,37 +5,16 @@ logger = logging.getLogger()
 
 class Solution:
     def duplicateZeros(self, arr: list[int]) -> None:
+        zeros_count = arr.count(0)
         size = len(arr)
-        logger.debug(f"{size = }, {arr = }")
-        if size < 2:
-            return
 
-        # Simulate copy to determine source and destination indices
-        # for copying from right to left
-        source = 0
-        dest = 0
-        for source, value in enumerate(arr):
-            dest += 1
-            if value == 0:
-                dest += 1
-
-            if dest >= size:
-                dest -= 1
-                break
-
-        logger.debug(f"{source = }, {dest = }")
-
-        # Actual copy, take care not to copy out of bound
-        while source >= 0:
-            # copy
-            if dest < size:
+        for source in range(size - 1, -1, -1):
+            # Copy
+            if (dest := source + zeros_count) < size:
                 arr[dest] = arr[source]
-            dest -= 1
 
-            # copy again if source contains 0
+            # Copy again if source contains zero
             if arr[source] == 0:
-                if dest < size:
-                    arr[dest] = arr[source]
-                dest -= 1
-
-            source -= 1
+                zeros_count -= 1
+                if (dest := source + zeros_count) < size:
+                    arr[dest] = 0
