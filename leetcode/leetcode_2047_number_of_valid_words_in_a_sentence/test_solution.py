df30304372485810d7dc975937cdbf4d8512023c
Hai Vu <haivu2004@gmail.com>
2026-09-18 22:05:18 -0700

    feat: add leetcode_2047_number_of_valid_words_in_a_sentence

---
+++ leetcode/leetcode_2047_number_of_valid_words_in_a_sentence/test_solution.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/number-of-valid-words-in-a-sentence
+"""
+
+import pytest
+
+from solution import Solution
+
+
+@pytest.mark.parametrize(
+    "sentence, expected",
+    [
+        pytest.param("cat and  dog", 3, id="Example 1"),
+        pytest.param("!this  1-s b8d!", 0, id="Example 2"),
+        pytest.param("alice and  bob are playing stone-game10", 5, id="Example 3"),
+        pytest.param("!", 1, id="punctuation only"),
+        pytest.param("a-b.", 1, id="dash in middle"),
+        pytest.param("a!", 1, id="punctuation at the end"),
+        pytest.param("c.,", 0, id="more than 1 punctuations"),
+    ],
+)
+def test_solution(sentence, expected):
+    sol = Solution()
+    assert sol.countValidWords(sentence) == expected
