43e1349350da004d480011337414ace1a0694bf7
Hai Vu <haivu2004@gmail.com>
2026-09-20 16:12:17 -0700

    chore: fix many incorrect test cases

--- leetcode/leetcode_2047_number_of_valid_words_in_a_sentence/solution.py
+++ leetcode/leetcode_2047_number_of_valid_words_in_a_sentence/solution.py
@@ -4,28 +4,51 @@ import string
 logger = logging.getLogger("solution")
 
 INVALID_FIRST_CHARS = set("-!,.")
-DASH = "-"
+HYPHEN = "-"
 VALID_CHARS = set(string.ascii_lowercase + "-!,.")
 PUNCTUATIONS = set("!,.")
 
 
 def is_valid(word: str):
-    counter = {}
+    # breakpoint()
+    # Check: At most one hypen and it must be sourrounded by lowercase characters
+    hypen_count = word.count(HYPHEN)
+    if hypen_count > 1:
+        logger.debug(f"Too many hypens: {word!r}")
+        return 0
+    elif hypen_count == 1:
+        try:
+            i = word.index(HYPHEN)
+            assert (
+                word[i - 1] in string.ascii_lowercase
+                and word[i + 1] in string.ascii_lowercase
+            )
+        except (AssertionError, IndexError):
+            logger.debug(f"Hypen not surrounded by lower case letter: {word!r}")
+            return 0
+
+    punctuations_count = 0
     last_index = len(word) - 1
+    valid = 1
+
     for i, ch in enumerate(word):
         valid = 0
         if ch not in VALID_CHARS:
+            logger.debug(f"Not a valid char: {ch!r}")
             break
 
-        if i == 0 and ch in INVALID_FIRST_CHARS:
+        if i == 0 and i != last_index and ch in INVALID_FIRST_CHARS:
+            logger.debug(f"Not a valid first char: {ch!r}")
             break
 
-        counter[ch] = counter.setdefault(ch, 0) + 1
-        if ch in PUNCTUATIONS and counter[ch] > 1:
-            break
-
-        if ch == DASH and (counter[ch] > 1 or i == 0 or i == last_index):
-            break
+        if ch in PUNCTUATIONS:
+            if i != last_index:
+                logger.debug(f"Punctuation not at the end of word: {word!r}")
+                break
+            punctuations_count += 1
+            if punctuations_count > 1:
+                logger.debug(f"Too many punctuations, {punctuations_count=}, {i=}")
+                break
 
         valid = 1
 
--- leetcode/leetcode_2047_number_of_valid_words_in_a_sentence/test_solution.py
+++ leetcode/leetcode_2047_number_of_valid_words_in_a_sentence/test_solution.py
@@ -14,9 +14,14 @@ from solution import Solution
         pytest.param("!this  1-s b8d!", 0, id="Example 2"),
         pytest.param("alice and  bob are playing stone-game10", 5, id="Example 3"),
         pytest.param("!", 1, id="punctuation only"),
-        pytest.param("a-b.", 1, id="dash in middle"),
         pytest.param("a!", 1, id="punctuation at the end"),
         pytest.param("c.,", 0, id="more than 1 punctuations"),
+        pytest.param("cn,p ", 0, id="comma inside word"),
+        # Hyphens
+        pytest.param("a-!b", 0, id="hyphen and punctuation together"),
+        pytest.param("a-b.", 1, id="hyphen in middle"),
+        pytest.param("foo-", 0, id="hyphen at the end"),
+        pytest.param("-foo", 0, id="hyphen as first char not allowed"),
     ],
 )
 def test_solution(sentence, expected):
