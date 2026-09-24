8a8196a5b76e6d7c9e8b2f95d909e665abe08505
Hai Vu <haivu2004@gmail.com>
2026-09-23 17:18:58 -0700

    refactor: improve performance in leetcode-2043

--- leetcode/leetcode_2043_simple_bank_system/solution.py
+++ leetcode/leetcode_2043_simple_bank_system/solution.py
@@ -3,38 +3,36 @@ class Bank:
         self.bound = len(balance)
         self.bal = balance
 
-    def _validate_account(self, account):
-        account -= 1
-        assert 0 <= account <= self.bound
-        return account
-
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        ok = True
-        try:
-            account1 = self._validate_account(account1)
-            account2 = self._validate_account(account2)
-            assert self.bal[account1] >= money
+        ok = False
+        account1 -= 1
+        account2 -= 1
+
+        if (
+            (0 <= account1 < self.bound)
+            and (0 <= account2 < self.bound)
+            and self.bal[account1] >= money
+        ):
             self.bal[account1] -= money
             self.bal[account2] += money
-        except AssertionError:
-            ok = False
+            ok = True
+
         return ok
 
     def deposit(self, account: int, money: int) -> bool:
-        ok = True
-        try:
-            account = self._validate_account(account)
+        ok = False
+
+        account -= 1
+        if 0 <= account < self.bound:
             self.bal[account] += money
-        except AssertionError:
-            ok = False
+            ok = True
+
         return ok
 
     def withdraw(self, account: int, money: int) -> bool:
-        ok = True
-        try:
-            account = self._validate_account(account)
-            assert self.bal[account] >= money
+        account -= 1
+        ok = False
+        if (0 <= account < self.bound) and self.bal[account] >= money:
             self.bal[account] -= money
-        except AssertionError:
-            ok = False
+            ok = True
         return ok
