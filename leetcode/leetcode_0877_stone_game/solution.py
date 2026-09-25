6df888ad1081da19a997ecf0d238e105f7edf159
Hai Vu <haivu2004@gmail.com>
2026-09-25 10:58:24 -0700

    (not quite) fix performance leetcode_0877_stone_game

--- leetcode/leetcode_0877_stone_game/solution.py
+++ leetcode/leetcode_0877_stone_game/solution.py
@@ -1,18 +1,23 @@
+import functools
+
+
 class Solution:
     def stoneGame(self, piles: list[int]) -> bool:
-        def play(left, right):
+        @functools.lru_cache
+        def play(left, right, total):
             if left == right:
                 return piles[left]
             elif left + 1 == right:
                 return max(piles[left], piles[right])
             else:
-                path1 = (
-                    piles[left]
-                    + sum(piles[left + 1 : right + 1])
-                    - play(left + 1, right)
-                )
-                path2 = piles[right] + sum(piles[left:right]) - play(left, right - 1)
+                path1 = total - play(left + 1, right, total - piles[left])
+                if path1 > threshold:
+                    return path1
+                path2 = total - play(left, right - 1, total - piles[right])
+                if path2 > threshold:
+                    return path2
                 return max(path1, path2)
 
-        threshold = sum(piles) // 2
-        return play(0, len(piles) - 1) > threshold
+        max_score = sum(piles)
+        threshold = max_score // 2
+        return play(0, len(piles) - 1, max_score) > threshold
--- leetcode/leetcode_0877_stone_game/test_solution.py
+++ leetcode/leetcode_0877_stone_game/test_solution.py
@@ -2,9 +2,17 @@
 https://leetcode.com/problems/stone-game/
 """
 
+import json
+
 import pytest
 
 from solution import Solution
+
+
+def param(filename):
+    with open(filename) as stream:
+        t = json.load(stream)
+    return pytest.param(t["piles"], t["expected"], id=t["id"])
 
 
 @pytest.mark.parametrize(
@@ -12,62 +20,8 @@ from solution import Solution
     [
         pytest.param([5, 3, 4, 5], True, id="Example 1"),
         pytest.param([3, 7, 2, 3], True, id="Example 2"),
-        pytest.param(
-            [
-                7,
-                7,
-                12,
-                16,
-                41,
-                48,
-                41,
-                48,
-                11,
-                9,
-                34,
-                2,
-                44,
-                30,
-                27,
-                12,
-                11,
-                39,
-                31,
-                8,
-                23,
-                11,
-                47,
-                25,
-                15,
-                23,
-                4,
-                17,
-                11,
-                50,
-                16,
-                50,
-                38,
-                34,
-                48,
-                27,
-                16,
-                24,
-                22,
-                48,
-                50,
-                10,
-                26,
-                27,
-                9,
-                43,
-                13,
-                42,
-                46,
-                24,
-            ],
-            True,
-            id="timeout",
-        ),
+        param("timeout1.json"),
+        param("timeout2.json"),
     ],
 )
 def test_solution(piles, expected):
